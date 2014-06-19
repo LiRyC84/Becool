8506e4489ad480839ef8fd777d289649c5cc2560
liryc <chpro84@gmail.com>
2014-06-19 10:58:08 +0200

    Projet Becool avec gestion utilisateur ok et authentification in_memory fonctionnelle

---
+++ app/cache/dev/twig/f9/eb/a9710737abff3695a7dc966baa5ea8246d7f405d0389b20a30c0ace82173.php
@@ -0,0 +1,132 @@
+<?php
+
+/* BecoolMainBundle:User:index.html.twig */
+class __TwigTemplate_f9eba9710737abff3695a7dc966baa5ea8246d7f405d0389b20a30c0ace82173 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "<h1>User list</h1>
+
+    <table class=\"records_list\">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Username</th>
+                <th>Salt</th>
+                <th>Password</th>
+                <th>Email</th>
+                <th>Isactive</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        ";
+        // line 19
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["entities"]) ? $context["entities"] : $this->getContext($context, "entities")));
+        foreach ($context['_seq'] as $context["_key"] => $context["entity"]) {
+            // line 20
+            echo "            <tr>
+                <td><a href=\"";
+            // line 21
+            echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("admin_user_show", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
+            echo "\">";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"), "html", null, true);
+            echo "</a></td>
+                <td>";
+            // line 22
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "username"), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 23
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "salt"), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 24
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "password"), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 25
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "email"), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 26
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "isActive"), "html", null, true);
+            echo "</td>
+                <td>
+                <ul>
+                    <li>
+                        <a href=\"";
+            // line 30
+            echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("admin_user_show", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
+            echo "\">show</a>
+                    </li>
+                    <li>
+                        <a href=\"";
+            // line 33
+            echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("admin_user_edit", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
+            echo "\">edit</a>
+                    </li>
+                </ul>
+                </td>
+            </tr>
+        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['entity'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 39
+        echo "        </tbody>
+    </table>
+
+        <ul>
+        <li>
+            <a href=\"";
+        // line 44
+        echo $this->env->getExtension('routing')->getPath("admin_user_new");
+        echo "\">
+                Create a new entry
+            </a>
+        </li>
+    </ul>
+    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "BecoolMainBundle:User:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  109 => 44,  102 => 39,  90 => 33,  84 => 30,  77 => 26,  73 => 25,  69 => 24,  65 => 23,  61 => 22,  55 => 21,  52 => 20,  48 => 19,  31 => 4,  28 => 3,);
+    }
+}
---
+++ src/Becool/Bundle/MainBundle/BecoolMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Becool\Bundle\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BecoolMainBundle extends Bundle
+{
+}
